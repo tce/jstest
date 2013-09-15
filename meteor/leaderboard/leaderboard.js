166cebfd0e9d2c3546e191e360fafadfd9f50b27
Thomas C Eskridge <teskridge@ihmc.us>
2013-09-14 20:11:08 -0400

    updates for mira integration

--- meteor/leaderboard/leaderboard.js
+++ meteor/leaderboard/leaderboard.js
@@ -1,17 +1,17 @@
 // Set up a collection to contain mira environment information. On the server,
 // it is backed by a MongoDB collection named "environment".
 
-Envs = new Meteor.Collection("environments");
-Agts = new Meteor.Collection("agents");
+Envs = new Meteor.Collection("registry");
+Agts = new Meteor.Collection("agentproperties");
 
 if (Meteor.isClient) {
     Template.leaderboard.env = function () {
-        return Envs.find({}, {sort: {numAgents: -1, name: 1}});
+        return Envs.find({}, {sort: {mira_label: 1}});
     };
 
     Template.leaderboard.selected_name = function () {
         var env = Envs.findOne(Session.get("selected_env"));
-        return env && env.name;
+        return env && env.mira_label;
     };
 
     Template.env.selected = function () {
@@ -33,10 +33,23 @@ if (Meteor.isClient) {
     // the leaderboard agt's will be those items in the Agts collection that have
     // "containedin": the selected environment
     Template.leaderboard.agt = function(){
-        return Agts.find({containedin: Envs.findOne(Session.get("selected_env")).name},{sort: {processing: -1, name:1}})
+        var agtHostId = Envs.findOne(Session.get("selected_env")).prop_key_env_id;
+        console.log(agtHostId);
+//        return Agts.find({prop_key_agt_host_env_id: agtHostId},{sort: {agent_name:1}})
+        var agtsInEnv = Agts.find({prop_key_agt_host_env_id: agtHostId},{sort: {agent_name:1}});
+        return agtsInEnv;
     }
 
+//    Template.cubegraphs.rendered = function(){
+//        var context = cubism.context().step(6e4).size(400).stop();
+//        d3.select("#cube-area svg").append("div")
+//            .attr("class","rule")
+//            .call(context.rule());
+//        d3.select("#cube-area svg").append("p").text("hello cubism");
+//    }
+
     Template.mainbody.rendered = function(){
+
         var width = 600;
         var height = 150;
 
@@ -45,17 +58,25 @@ if (Meteor.isClient) {
             .attr("height", height);
 
         var selectedvar = Envs.findOne(Session.get("selected_env"));
-        var selectedEnv = selectedvar && selectedvar.name;
+        console.log(selectedvar);
+
+        var selectedEnv = selectedvar && selectedvar.mira_label;
 
         console.log(selectedEnv);
-        var data = Envs.find({}, {sort: {numAgents: -1, name: 1}}).fetch();
-        var circles = svg.selectAll("circle").data(data);
+        var data = Envs.find({}, {sort: {numAgents: -1, mira_label: 1}}).fetch();
+        var agtdata = selectedvar && Agts.find({prop_key_agt_host_env_id: selectedvar.prop_key_env_id},{sort: {agent_name:1}});
+        var environmentAgents = selectedvar && selectedvar.agtlist;
+        console.log(environmentAgents);
 
+
+        // draw the circles for the MIRA environments
+
+        var circles = svg.selectAll("circle").data(data);
 
         circles
             .attr("cx", function(d,i) { return i*50 + 50;})
             .attr("cy", height/2)
-            .attr("fill", function(d){ if (d.name == selectedEnv)
+            .attr("fill", function(d){ if (d.mira_label == selectedEnv)
             {return "yellow"}
             else
             {return d3.rgb(180, 180, 180)}})
@@ -67,7 +88,7 @@ if (Meteor.isClient) {
         circles.enter().append("svg:circle")
             .attr("cx", function(d,i) { return i*50 + 50;})
             .attr("cy", height/2)
-            .attr("fill", function(d){ if (d.name == selectedEnv)
+            .attr("fill", function(d){ if (d.mira_label == selectedEnv)
             {return "yellow"}
             else
             {return d3.rgb(180, 180, 180)}})
@@ -79,23 +100,26 @@ if (Meteor.isClient) {
         circles.exit().transition().attr("r",0).remove();
 
         var labels = svg.selectAll("text").data(data)
-            .text(function(d){return d.name;})
+            .text(function(d){return d.mira_label;})
             .attr("x", function(d,i) {return i*50 + 40;})
             .attr("y", height/2+4)
             .attr("font-family","sans-serif")
             .attr("font-size", "11px")
-            .attr("fill", function(d){ if (d.name == selectedEnv) {return "black"} else {return "white"}});
+            .attr("fill", function(d){ if (d.mira_label == selectedEnv) {return "black"} else {return "white"}});
 
 
         labels.enter().append("text")
-            .text(function(d){return d.name;})
+            .text(function(d){return d.mira_label;})
             .attr("x", function(d,i) {return i*50 + 40;})
             .attr("y", height/2+4)
             .attr("font-family","sans-serif")
             .attr("font-size", "11px")
-            .attr("fill", function(d){ if (d.name == selectedEnv) {return "black"} else {return "white"}});
+            .attr("fill", function(d){ if (d.mira_label == selectedEnv) {return "black"} else {return "white"}});
 
         labels.exit().remove();
+
+
+        //
     }
 
 }
