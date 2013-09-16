1c611885c8631d500485aa0db1fddc240b39bb0c
Thomas C Eskridge <teskridge@ihmc.us>
2013-09-15 22:46:21 -0400

    latest constellation

--- meteor/leaderboard/leaderboard.js
+++ meteor/leaderboard/leaderboard.js
@@ -122,176 +122,244 @@ if (Meteor.isClient) {
 
     Template.constellation.rendered = function()
     {
-        var w = window.innerWidth - 60;
-        var h = window.innerHeight - 60;
-        var color = d3.scale.category20();
+	var w = window.innerWidth - 10;
+	var h = window.innerHeight - 10;
+	var radius = h*0.6;
+	var color = d3.scale.category20();
+	var rings = d3.scale.category20c();
         var svg = d3.select("#constellation-area svg")
-                .attr("width", w)
-                .attr("height", h);
+	.attr("width", w)
+	.attr("height", h);
         var g = svg.append("g");
-        var environments = [];
-        var sectors = [0];
+	var g = svg.append("g")
+	.attr("transform","translate(" + w/2 + "," + h/2 + ")");
+	var environments = [];
+	var sectors = [0];
+	var segments = [];
 
-        function graph() {
-            var force = d3.layout.force();
-            var nodes = force.nodes();
-            var links = force.links();
+	var arc = d3.svg.arc();
 
-            this.addEnvironment = function(id, ip) {
-                for(var i=0; i<environments.length; i++){
-                    if(ip < environments[i].ip){
-                        environments.splice(i,0,{"id": id, "ip": ip, "agents": []});
-                        return;
-                    }
-                }
-                environments.push({"id": id, "ip": ip, "agents": []});
-            }
+	setInterval(function(){
+		var cur;
+		var n = 0;
+		var region;
+		var layers = 0;
+		for(var i=0; i<environments.length; i++){
+		    if(environments[i].ippath.length > layers){
+			layers = environments[i].ippath.length;
+		    }
+		}
+		console.log(layers);
+		for(layer=0; layer<layers; layer++){
+		    cur = environments[n].ippath[layer];
+		    region = (radius * 0.65) + 17 * (5 - (layer + 1));
+		    segments[n] = g.append("path")
+			.datum({innerRadius: region, outerRadius: region*1.04, startAngle: Math.PI/2, endAngle: Math.PI/2})
+			.style("fill",rings(n))
+			.attr("d",arc);
+		    var start = Math.PI/2;
+		    var end = Math.PI/2;
+		    for(var j=0; j<environments.length; j++){
+			if(cur != environments[j].ippath[layer]){
+			    end = sectors[j] + Math.PI/2;
+			    segments[n].transition().duration(2000).call(arcTween, end);
+			    cur = environments[j].ippath[layer];
+			    start = end;
+			    n++;
+			    segments[n] = g.append("path")
+				.datum({innerRadius: region, outerRadius: region*1.04, startAngle: start, endAngle: end})
+				.style("fill",rings(n))
+				.attr("d",arc);
+			}
+		    }
+		    end = sectors[j] + Math.PI/2;
+		    segments[n].transition().duration(2000).call(arcTween, end);
+		    n++;
+		}
+	    },1000);
 
-            this.addAgent = function(id, env) {
-                if(typeof env != "number"){
-                    env = findEnvironmentIndex(env) + 1;
-                };
-                nodes.push({"id": id, "env": env, "ith": environments[env].agents.length + 1});
-                environments[env].agents.push(findAgent(id));
-                update();
-            }
+	function arcTween(transition, newAngle) {
+	    transition.attrTween("d",function(d){
+		    var interpolate = d3.interpolate(d.endAngle, newAngle);
+		    return function(t) {
+			d.endAngle = interpolate(t);
+			return arc(d);
+		    }
+		})
+		}
 
-            this.removeAgent = function(id) {
-                var i = 0;
-                var n = findAgent(id);
-                while(i < links.length){
-                    if ((links[i]['source'] == n) || (links[i]['target'] == n)) links.splice(i, 1);
-                    else i++;
-                }
-                nodes.splice(findAgentIndex(id), 1);
-                update();
-            }
+	function graph() {
+	    var force = d3.layout.force();
+	    var nodes = force.nodes();
+	    var links = force.links();
 
-            this.addLink = function(source, target, value) {
-                links.push({"source": findAgent(source), "target": findAgent(target), "value": value});
-                update();
-            }
+	    this.addEnvironment = function(name, ip) {
+		var ippath = ip.split(".");
+		ippath.splice(ippath.length-1,1);
+		for(var i=0; i<environments.length; i++){
+		    for(var j=0; j<ippath.length; j++){
+			if(ippath[j] > environments[i].ippath[j]){
+			    break;
+			}else
+			    if(ippath[j] < environments[i].ippath[j]){
+				environments.splice(i,0,{"id": name, "ippath": ippath, "agents": []});
+				return;
+			    }
+		    }
+		}
+		environments.push({"id": name, "ippath": ippath, "agents": []});
+	    }
 
-            var findEnvironmentIndex = function(id) {
-                for (var i in environments) {if (environments[i]["id"] === id) return i-1};
-            }
+	    this.addAgent = function(id, env) {
+		if(typeof env != "number"){
+		    env = findEnvironmentIndex(env) + 1;
+		};
+		nodes.push({"id": id, "env": env, "ind": environments[env].agents.length + 1});
+		environments[env].agents.push(findAgent(id));
+		update();
+	    }
 
-            var findAgent = function(id) {
-                for(var i in nodes){ if(nodes[i]["id"] === id) return nodes[i]};
-            }
+	    this.removeAgent = function(id) {
+		var i = 0;
+		var n = findAgent(id);
+		while(i < links.length){
+		    if ((links[i]['source'] == n) || (links[i]['target'] == n)) links.splice(i, 1);
+		    else i++;
+		}
+		nodes.splice(findAgentIndex(id), 1);
+		update();
+	    }
 
-            var findAgentIndex = function(id) {
-                for (var i in nodes) {if (nodes[i]["id"] === id) return i};
-            }
+	    this.addLink = function(source, target, value) {
+		links.push({"source": findAgent(source), "target": findAgent(target), "value": value});
+		update();
+	    }
 
-            var update = function () {
-                var link = g.selectAll(".link")
-                        .data(links, function(d) { return d.source.id + "&" + d.target.id; });
+	    var findEnvironmentIndex = function(id) {
+		for (var i in environments) {if (environments[i]["id"] === id) return i-1};
+	    }
 
-                link.enter().insert("line")
-                        .attr("id",function(d){return d.source.id + "&" + d.target.id;})
-                        .attr("stroke",function(d){
-                            return color(findAgentIndex(d.source.id));
-                        })
-                        .attr("stroke-width",1)
-                        .attr("class","link");
+	    var findAgent = function(id) {
+		for(var i in nodes){ if(nodes[i]["id"] === id) return nodes[i]};
+	    }
 
-                link.transition().delay(function(d){ return d.value }).remove(); //optional remove on timer
+	    var findAgentIndex = function(id) {
+		for (var i in nodes) {if (nodes[i]["id"] === id) return i};
+	    }
 
-                link.exit().remove();
+	    var update = function () {
+		var link = g.selectAll(".link")
+		.data(links, function(d) { return d.source.id + "&" + d.target.id; });
 
-                var node = svg.selectAll(".node")
-                        .data(nodes, function(d){ return d.id });
+		link.enter().insert("line")
+		.attr("id",function(d){return d.source.id + "&" + d.target.id;})
+		.attr("stroke",function(d){
+                        return color(findAgentIndex(d.source.id));
+                    })
+		.attr("stroke-width",1)
+		.attr("class","link");
 
-                var nodeEnter = node.enter().append("g")
-                        .attr("class", "node")
-                        .call(force.drag);
+		link.transition().delay(function(d){ return d.value }).remove(); //optional remove on timer
 
-                nodeEnter.append("rect")
-                        .attr("width",2)
-                        .attr("height",2)
-                        .attr("x",0)
-                        .attr("y",0)
-                        .attr("fill",function(d,i){ return color(d.env) })
-                        .attr("id", function(d){ return "Node;" + d.id });
+		link.exit().remove();
 
-                node.exit().remove();
+		var node = svg.selectAll(".node")
+		.data(nodes, function(d){ return d.id });
 
-                for(var i=1; i<=environments.length; i++){
-                    sectors[i] = 0;
-                    for(var j=0; j<i; j++){
-                        sectors[i] += environments[j].agents.length;
-                    }
-                }
-                for(var i=1; i<sectors.length; i++){
-                    sectors[i] = sectors[i] / nodes.length * 2 * Math.PI;
-                }
+		var nodeEnter = node.enter().append("g")
+		.attr("class", "node")
+		.call(force.drag);
 
-                var label = svg.selectAll(".label")
-                        .data(environments, function(d,i){ return i });
+		nodeEnter.append("rect")
+		.attr("width",2)
+		.attr("height",2)
+		.attr("x",0)
+		.attr("y",0)
+		.attr("fill",function(d,i){ return color(d.env) })
+		.attr("id", function(d){ return "Node;" + d.id });
 
-                label.enter().append("text")
-                        .text(function(d,i){ return "♦ENV" + i })
-                        .attr("class","label")
-                        .attr("fill",function(d,i){ return color(i) })
-                        .attr("text-anchor","middle")
-                        .style("font-size","10px");
+		node.exit().remove();
 
-                label.exit().remove();
+		for(var i=1; i<=environments.length; i++){
+		    sectors[i] = 0;
+		    for(var j=0; j<i; j++){
+			sectors[i] += environments[j].agents.length;
+		    }
+		}
+		for(var i=1; i<sectors.length; i++){
+		    sectors[i] = sectors[i] / nodes.length * 2 * Math.PI;
+		}
 
-                force.on("tick", function(e) {
-                    link    .attr("x1", function(d) { return d.source.x; })
-                            .attr("y1", function(d) { return d.source.y; })
-                            .attr("x2", function(d) { return d.target.x; })
-                            .attr("y2", function(d) { return d.target.y; });
+		var label = svg.selectAll(".label")
+		.data(environments);
 
-                    var monitoredValue = 500;
+		label.enter().append("text")
+		.text(function(d,i){ return "♦ENV" + i })
+		.attr("class","label")
+		.attr("fill",function(d,i){ return color(i) })
+		.attr("text-anchor","middle")
+		.style("font-size","10px");
 
-                    var k = e.alpha * 0.1;
-                    nodes.forEach(function(o,i) {
-                        o.x += (w / 2 + Math.cos(sectors[o.env] + (o.ith / environments[o.env].agents.length) * (sectors[o.env+1] - sectors[o.env])) * monitoredValue - o.x) * k;
-                        o.y += (h / 2 + Math.sin(sectors[o.env] + (o.ith / environments[o.env].agents.length) * (sectors[o.env+1] - sectors[o.env])) * monitoredValue - o.y) * k;
-                    });
+		label.exit().remove();
 
-                    node    .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; });
+		force.on("tick", function(e) {
+			link    .attr("x1", function(d) { return d.source.x; })
+			    .attr("y1", function(d) { return d.source.y; })
+			    .attr("x2", function(d) { return d.target.x; })
+			    .attr("y2", function(d) { return d.target.y; });
 
-                    label   .attr("x",function(d,i){ return w/2 + Math.cos((sectors[i] + sectors[i+1]) / 2) * 300 })
-                            .attr("y",function(d,i){ return h/2 + Math.sin((sectors[i] + sectors[i+1]) / 2) * 300 })
-                });
+			var monVal = 0;
+
+			var k = e.alpha * 0.1;
+			nodes.forEach(function(o,i) {
+				o.x += (w / 2 + Math.cos(sectors[o.env] + (o.ind / environments[o.env].agents.length) * (sectors[o.env+1] - sectors[o.env])) * (radius - monVal) - o.x) * k;
+				o.y += (h / 2 + Math.sin(sectors[o.env] + (o.ind / environments[o.env].agents.length) * (sectors[o.env+1] - sectors[o.env])) * (radius - monVal) - o.y) * k;
+			    });
+
+			node    .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; });
+
+			label   .attr("x",function(d,i){ return w/2 + Math.cos((sectors[i] + sectors[i+1]) / 2) * (radius * 0.6) })
+			    .attr("y",function(d,i){ return h/2 + Math.sin((sectors[i] + sectors[i+1]) / 2) * (radius * 0.6) })
+			    });
 
 
-                force
-                        .linkStrength(0)
-                        .linkDistance(50)
-                        .charge(-2)
-                        .size([w,h])
-                        .start();
-            }
-            update();
-        }
+		force
+		.linkStrength(0)
+		.linkDistance(50)
+		.charge(-2)
+		.size([w,h])
+		.start();
+	    }
+	    update();
+	}
 
-        var graph = new graph();
+	var graph = new graph();
 
-        graph.addEnvironment("blue",3); //normally id and parent ip would be easily extracted from same string, like .101 <-- --> 172.16.1
-        graph.addEnvironment("light",2);
-        graph.addEnvironment("orange",2);
-        graph.addEnvironment("tan",1);
+	graph.addEnvironment("first","172.16.2.101");
+	graph.addEnvironment("second","172.16.1.101");
+	graph.addEnvironment("third","172.16.2.102");
+	graph.addEnvironment("fourth","172.16.1.102");
+	graph.addEnvironment("fifth","200.1");
 
-        for(var i=0; i<21; i++){
-            graph.addAgent(i,"blue");
-        }
+	for(var i=0; i<21; i++){
+	    graph.addAgent(i,"first");
+	}
 
-        for(var i=21; i<28; i++){
-            graph.addAgent(i,"light");
-        }
+	for(var i=21; i<28; i++){
+	    graph.addAgent(i,"second");
+	}
 
-        for(var i=28; i<100; i++){
-            graph.addAgent(i,"orange");
-        }
+	for(var i=28; i<100; i++){
+	    graph.addAgent(i,"third");
+	}
 
-        for(var i=100; i<150; i++){
-            graph.addAgent(i,"tan");
-        }
+	for(var i=100; i<150; i++){
+	    graph.addAgent(i,"fourth");
+	}
+
+	graph.addAgent("green guy","fifth");
+
+	console.log(environments);
 
     }
 
